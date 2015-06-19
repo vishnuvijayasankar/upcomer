f4a5f50a776ee4bf92948703614cf15acd9b6a0b
VISHNU <vishnuvijayasankar@qburst.com>
2015-06-19 17:51:59 +0530

    fully responsive

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,5 +1,5 @@
 $('#down-arrow').click(function() {
    //optionally remove the 500 (which is time in milliseconds) of the
    //scrolling animation to remove the animation and make it instant
-   $.scrollTo($('#down-arrow'), 500);
+   $.scrollTo($('#section-2'), 500);
 });
